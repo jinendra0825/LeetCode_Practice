de81a5ed73e696de895522a0554dccd58a30dab6
Jinendra <151931071+jinendra0825@users.noreply.github.com>
2026-02-21 14:14:13 +0530

    Time: 2 ms (95.08%) | Memory: 45.9 MB (33.77%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,30 +1,41 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] arr, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> comb = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+       List<List<Integer>> ans = new ArrayList<>();
+       List<Integer> comb = new ArrayList<>();
 
-        combinations(arr, 0 , target , ans , comb);    
+       combination(candidates , 0, target, ans , comb );
 
-        return ans;
+       return ans;
+
+
     }
 
-    public static void combinations(int arr[] , int i, int target, List<List<Integer>> ans, List<Integer> comb ){
+    public static void combination(int arr[] , int ind, int target, List<List<Integer>> ans, List<Integer> comb){
+
         // Base condition
-        if(i == arr.length){
+        if(ind == arr.length){
             if(target == 0){
-               ans.add(new ArrayList<>(comb));
+                ans.add(new ArrayList(comb));
+                return;
             }
+
             return;
+
         }
 
-       if(arr[i] <= target){
-        comb.add(arr[i]);
-
-        combinations(arr , i , target - arr[i] , ans, comb);
-
+        // Take Condition
+        if(arr[ind] <= target){
+        comb.add(arr[ind]);
+        combination(arr, ind, target - arr[ind] , ans , comb);
+       
         comb.remove(comb.size() - 1);
-       }
+        }
 
-       combinations(arr, i + 1, target, ans, comb);
+        // NotTake
+        combination(arr, ind + 1 , target , ans , comb);
+
+
     }
+
+
 }
