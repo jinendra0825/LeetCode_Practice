da3ca535e3974efbe5ca6855e133e9cd6868e297
Jinendra <151931071+jinendra0825@users.noreply.github.com>
2026-02-24 21:53:09 +0530

    Time: 2 ms (97.81%) | Memory: 45.4 MB (83.98%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        int n = s.length();
+        int[] hash = new int[256];
+        Arrays.fill(hash, -1);
+
+        int l = 0, r = 0;
+        int maxlen = 0;
+
+        while (r < n) {
+
+            char ch = s.charAt(r);
+
+            // agar character already window me hai
+            if (hash[ch] != -1 && hash[ch] >= l) {
+                l = hash[ch] + 1;
+            }
+
+            // current index update karo
+            hash[ch] = r;
+
+            // window length
+            int len = r - l + 1;
+            maxlen = Math.max(maxlen, len);
+
+            r++; // IMPORTANT: right pointer move karo
+        }
+
+        return maxlen;
+    }
+}
