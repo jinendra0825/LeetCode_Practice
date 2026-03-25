6cfd56ffaadcd840d47f1decf8492f6d3da090a4
Jinendra <151931071+jinendra0825@users.noreply.github.com>
2026-03-25 13:58:25 +0530

    Time: 2 ms (98.13%) | Memory: 48.1 MB (95.66%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
@@ -25,7 +25,7 @@ class Solution {
 
         while(!queue.isEmpty()){
             int size = queue.size();
-            List<TreeNode> currlevel = new ArrayList<>(size);
+            
 
             double avg = 0.0;
             for(int i = 0 ; i < size ; i++){
