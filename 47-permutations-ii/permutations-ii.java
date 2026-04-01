175ec420e7b9d26b39f8a330d3b1923ce89862af
Jinendra <151931071+jinendra0825@users.noreply.github.com>
2026-04-01 13:21:07 +0530

    Time: 714 ms (5.06%) | Memory: 46.7 MB (54.55%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+         List<List<Integer>> ans = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        combination(nums, used,  new ArrayList<>(), ans);
+        return ans;
+    }
+
+     public static void combination(int arr[] , boolean used[],  List<Integer> comb, List<List<Integer>> ans){
+
+        // Base Condition --> 
+
+        if(comb.size() == arr.length){
+
+            if(!ans.contains(comb)){
+                 ans.add(new ArrayList<>(comb));
+            return;
+            }
+            
+        }
+
+        for(int i = 0 ; i < arr.length; i++){
+
+            if(used [i]) continue;
+
+            comb.add(arr[i]);
+            used[i] = true;
+            combination(arr, used,  comb, ans);
+
+
+            comb.remove(comb.size() - 1);
+            used[i] = false ;
+        
+        }
+
+        return;
+
+
+    }
+
+}
