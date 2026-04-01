a697f530ae1ae43a438e543053e0e33e1f537d44
Jinendra <151931071+jinendra0825@users.noreply.github.com>
2026-04-01 16:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (42.14%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numTrees(int n) {
+        long result = 1;
+
+        for(int i= 0 ; i < n ; i++){
+            result = result * 2 * ( 2 * i + 1) / (i + 2);
+        }
+    return (int) result;
+    }
+
+}
