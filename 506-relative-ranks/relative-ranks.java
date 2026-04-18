13bf5226ef6fc9e4c38d58bca3cf2d154330de14
Jinendra <151931071+jinendra0825@users.noreply.github.com>
2026-04-18 12:56:32 +0530

    Time: 10 ms (69.92%) | Memory: 47.2 MB (93.05%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+
+        
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = score[i]; // score
+            arr[i][1] = i;        // original index
+        }
+
+        
+        Arrays.sort(arr, (a, b) -> b[0] - a[0]); //Array ko descending order mai sort kiya hai -> a[0][], b[0][], first element ke hisab se dec mai sort kiya hai
+
+        
+        String[] ans = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            int originalIndex = arr[i][1];
+
+            if (i == 0) {
+                ans[originalIndex] = "Gold Medal";
+            } else if (i == 1) {
+                ans[originalIndex] = "Silver Medal";
+            } else if (i == 2) {
+                ans[originalIndex] = "Bronze Medal";
+            } else {
+                ans[originalIndex] = String.valueOf(i + 1);
+            }
+        }
+
+        return ans;
+    }
+}
