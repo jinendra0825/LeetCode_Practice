1efb6c4e9e163bb29b3189974bfe725283a65cf4
Jinendra <151931071+jinendra0825@users.noreply.github.com>
2026-04-22 08:43:48 +0530

    Time: 6 ms (72.79%) | Memory: 43.6 MB (99.26%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> ans = new ArrayList<>();
+
+        for (String query : queries) {
+            boolean found = false;
+
+            for (String word : dictionary) {
+                int diff = 0;
+
+                for (int i = 0; i < query.length(); i++) {
+                    if (query.charAt(i) != word.charAt(i)) {
+                        diff++;
+                    }
+
+                    if (diff > 2) break;
+                }
+
+                if (diff <= 2) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                ans.add(query);
+            }
+        }
+
+        return ans;
+    }
+}
