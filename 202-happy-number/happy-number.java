2e37a56bb2db8d86cbe1ab4fe3a5c831e101f3ed
Jinendra <151931071+jinendra0825@users.noreply.github.com>
2026-04-27 12:19:47 +0530

    Time: 1 ms (80.07%) | Memory: 42.6 MB (23.35%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public boolean isHappy(int n) {
+        
+        HashSet<Integer> seen = new HashSet<>();
+
+        while (n != 1) {
+            if (seen.contains(n)) return false; // cycle detected
+            
+            seen.add(n);
+
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+
+        return true;
+    }
+}
